f225f4637791abdd25cfcdbb403791b90295f3a7
LielYoash <lielyoash76@gmail.com>
2022-11-24 14:24:40 +0200

    fixed spaces

--- main.c
+++ main.c
@@ -6,31 +6,35 @@ int main() {
     printf("Enter your wanted range: \n");
     scanf("%d %d", &start, &end);
 
-    printf("primes: ");
-    for (int i = start; i < end; i++) {
-        if(isPrime(i) == 1 && i>= start && i<=end)
-            printf("%d ", i);
-    }
-    printf("\n");
-
-    printf("strongs: ");
-    for (int i = start; i < end; i++) {
-        if(isStrong(i) == 1 && i>= start && i<=end)
-            printf("%d ", i);
-    }
-    printf("\n");
-
-    printf("palimdromes: ");
-    for (int i = 0; i <= end - start; i++) {
-        if(isPalindrome(i) == 1 && i>= start && i<=end)
-            printf("%d " , i);
-    }
-    printf("\n");
-
-    printf("armStrongs: ");
+    printf("The Armstrong numbers are:");
     for (int i = 0; i < end - start; i++) {
-        if(isArmstrong(i) == 1 && i>= start && i<=end)
-            printf("%d ", i);
+        if (isArmstrong(i) == 1 && i >= start && i <= end) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+
+    printf("The Palindromes are:");
+    for (int i = 0; i <= end - start; i++) {
+        if (isPalindrome(i) == 1 && i >= start && i <= end) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+
+    printf("The Prime numbers are:");
+    for (int i = start; i < end; i++) {
+        if (isPrime(i) == 1 && i >= start && i <= end) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+
+    printf("The Strong numbers are:");
+    for (int i = start; i < end; i++) {
+        if (isStrong(i) == 1 && i >= start && i <= end) {
+            printf(" %d", i);
+        }
     }
     printf("\n");
 
